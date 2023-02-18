0d0dc4cad420438a69d9cf57c77e15296b77a05b
Usha Varshini <varshini.uv@gmail.com>
2023-02-18 16:45:14 +0530

    Protected Routes

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import Products from "./pages/Products";
 import Navbar from "./components/Navbar";
 import Footer from './components/Footer'; 
 import { Auth, useAuth } from "@arcana/auth-react";
+import Protected from "./components/Protected";
 const contractAbi = require("./artifacts/contracts/Reviewed.sol/Reviewed.json").abi;
 
 function App() {
@@ -20,30 +21,42 @@ function App() {
   const auth = useAuth();
   const [contract, setContract] = useState(null);
   const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+  const isSignedIn = true;
 
-  useEffect(() => {
-    // Get the provider object from ethers.js
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
+  // useEffect(() => {
+  //   // Get the provider object from ethers.js
+  //   const provider = new ethers.providers.Web3Provider(window.ethereum);
     
-    // Get the signer object from the provider
-    const signer = provider.getSigner();
+  //   // Get the signer object from the provider
+  //   const signer = provider.getSigner();
     
-    // Get the contract address and instantiate the contract object
-    const contractTemp = new ethers.Contract(contractAddress, contractAbi, signer);
-    setContract(contractTemp); 
+  //   // Get the contract address and instantiate the contract object
+  //   const contractTemp = new ethers.Contract(contractAddress, contractAbi, signer);
+  //   setContract(contractTemp); 
     
-  }, []);
+  // }, []);
   
   
   return (
     <div className="App">
 
       <Router>
-      <Navbar auth={auth}/>
       <Routes>
         <Route exact path="/" element={<Landing />} />
-        <Route exact path="/Products" element={<Products />} />
-        <Route exact path="/Products/:deviceName" element={<Detailed />} />
+        <Route exact 
+          path="/Products" 
+          element={
+            <Protected isSignedIn={isSignedIn}>
+            <Products />
+            </Protected>
+          } />
+        <Route exact 
+          path="/Products/:deviceName" 
+          element={
+            <Protected isSignedIn={isSignedIn}>
+            <Detailed />
+            </Protected>
+            } />
         <Route path="*" element={<Landing />} />
       </Routes>
       </Router>
