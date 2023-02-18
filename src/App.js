000302dbf665b32b329a7c4cd14442096b15e451
barath99 <exploretechsbk@gmail.com>
2023-02-18 15:40:00 +0530

    Login fix

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import {
   BrowserRouter as Router,
   Route,
-  Link,
-  Routes
+  Routes,
+  Navigate,
+  useLocation,
 } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { ethers } from "ethers";
@@ -13,10 +14,6 @@ import Navbar from "./components/Navbar";
 import Footer from './components/Footer'; 
 import { Auth, useAuth } from "@arcana/auth-react";
 const contractAbi = require("./artifacts/contracts/Reviewed.sol/Reviewed.json").abi;
-
-const onLogin = () => {
-  // Route to authenticated page
-}
 
 function App() {
 
@@ -34,58 +31,21 @@ function App() {
     // Get the contract address and instantiate the contract object
     const contractTemp = new ethers.Contract(contractAddress, contractAbi, signer);
     setContract(contractTemp); 
+    
   }, []);
   
-  async function getReview(reviewId) {
-    try {
-      const review = await contract.getReview(1);
-      return review;
-    } catch (err) {
-      console.error(err);
-    }
-  }
-
-  async function addReview(review) {
-    if (typeof window.ethereum !== "undefined") {
-      // ethereum is usable, get reference to the contract
-      await requestAccount();
-      const provider = new ethers.providers.Web3Provider(window.ethereum);
-      // signer needed for transaction that changes state
-      const signer = provider.getSigner();
-      const contract = new ethers.Contract(contractAddress, contractAbi, signer);
-      // perform transaction
-      const transaction = await contract.createReview(review);
-      await transaction.wait();
-      return true;
-    } else {
-      console.error("Ethereum is not usable.");
-      return false;
-    }
-  }
-
-  async function requestAccount() {
-    await window.ethereum.request({method: 'eth_requestAccounts'});
-  }
-
   
-
   return (
     <div className="App">
 
       <Router>
-      <Navbar/>
-      
-        <Routes>
-          <Route exact path="/" element={<><div>
-          {auth.loading ? ("Loading") : auth.isLoggedIn ? (<p>Logged In</p>) : (
-            <div><Auth externalWallet={true} theme={"dark"} onLogin={onLogin}/></div>
-          )}
-        </div><Landing /><button onClick={()=>addReview("First Review ever...")}>Submit review</button>
-        </>}  />
-          <Route exact path="/Products/:deviceName" element={<Detailed/>} />
-          <Route exact path="/Products" element={<Products/>} />
-          <Route path="*" element={<Landing/>}/>
-        </Routes>
+      <Navbar auth={auth}/>
+      <Routes>
+        <Route exact path="/" element={<Landing />} />
+        <Route exact path="/Products" element={<Products />} />
+        <Route exact path="/Products/:deviceName" element={<Detailed />} />
+        <Route path="*" element={<Landing />} />
+      </Routes>
       </Router>
       <Footer/>
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,28 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom'
+import { Auth, useAuth } from "@arcana/auth-react";
 
-const Navbar = () => {
 
+const Navbar = ({auth}) => {
+  console.log("Loggedin? ",auth.isLoggedIn)
   const [SignInClicked, setSignInClicked] = useState(false);
-  const [user, setUser] = useState();
-  const [shortText, setShortText] = useState("");
+  const [AccountId, setAccountId] = useState("");
+  const [showModal, setShowModal] = useState(false);
+  
+  const handleSignInClick = async() => {
+    setShowModal(true);
+  };
+  
+  const handleModalClose = () => {
+    setShowModal(false);
+  };
 
   useEffect(() => {
     
   }, []);
 
   useEffect(() => {
-    // const connect = async () => {
-    //   let res = await connectToCeramic();
-    //   /** Check if connection is successful or not */
-    //   if(res.status == 200) {
-    //     setUser(res.did);
-    //     console.log(res)
-    //     setShortText(res.did.substring(0, 3) + "..." + res.did.substring(res.did.length - 5));
-    //   } else {
-    //     console.log("Error connecting to Ceramic: ", res);
-    //     alert("Error connecting to Ceramic.");
-    //   }
-    // }
-    // connect();
+ 
   }, [SignInClicked]); // specify the state variable here
 
   return (
@@ -41,20 +39,26 @@ const Navbar = () => {
             </div>
             </Link>
 
-            {!user &&
-            <span class="inline-flex rounded-md shadow-sm">
-                <button onClick={() => setSignInClicked(true)} class="inline-flex items-center justify-center px-4 py-2 text-base font-medium leading-6 text-white whitespace-no-wrap bg-blue-600 border border-blue-700 rounded-md shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500" data-rounded="rounded-md" data-primary="blue-600">
-                    Sign in
-                </button>
+            <span className="inline-flex rounded-md shadow-sm">
+            <button
+              className="inline-flex items-center justify-center px-4 py-2 text-base font-medium leading-6 text-white whitespace-no-wrap bg-blue-600 border border-blue-700 rounded-md shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+              onClick={handleSignInClick}
+            >
+              {(!auth.isLoggedIn && "Sign in") || "Log out"}
+            </button>
             </span>
-            }
-            {user &&
-            <span class="inline-flex rounded-md shadow-sm">
-                <div class="inline-flex items-center justify-center px-4 py-2 text-base font-medium leading-6 text-overflow-ellipsis overflow-hidden w-auto text-white whitespace-no-wrap bg-blue-600 border border-blue-700 rounded-md shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500" data-rounded="rounded-md" data-primary="blue-600">
-                    Welcome {shortText}
+
+            {showModal && (
+              <div
+                id="defaultModal"
+                className="fixed top-0 left-0 right-0 z-50 w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full"
+              >
+                <div className="bg-indigo-700 rounded-lg shadow-md pt-10 pb-8 m-auto w-96 relative">
+                  <button className="absolute top-4 right-4 mb-2 text-sm font-medium text-gray-200 " onClick={handleModalClose}>X</button>
+                  <Auth externalWallet={true} theme={"dark"} />
                 </div>
-            </span>
-            }
+              </div>
+            )}
         </div>
         </div>
     </section>
